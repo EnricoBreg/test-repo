42fe3b3583a847ab2a3f6b231366ede0defbd52b
EnricoBreg <bregolienrico@gmail.com>
2023-01-27 08:41:45 +0100

    Add folder Laracast

---
+++ Laracast/demo-app/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+    plugins: [
+        vue({
+            template: {
+                trasformAssetUrls: {
+                    base: null,
+                    includeAbsolute: null,
+                },
+            },
+        }),
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+});
